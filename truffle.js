4a3fb3534d32d72017c4dc3939cd1cc8f1df59b5
Bilous4singree <bilous@singree.com>
2018-10-26 13:01:19 +0300

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -3,7 +3,6 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
+      network_id: "*" // Match any network id 
   }
 };
